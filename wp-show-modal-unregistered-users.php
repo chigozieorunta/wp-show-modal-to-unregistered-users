80dd045436f586bf3d2c29026397d19fe6bdfa6d
chigozieorunta <chigozieorunta@yahoo.com>
2020-01-26 21:14:34 +0100

    Modified new plugin file

--- wp-show-modal-unregistered-users.php
+++ wp-show-modal-unregistered-users.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * Plugin Name: Indicate Register
- * Plugin URI:  https://github.com/chigozieorunta/woo-modify-order-address-details
- * Description: A simple woocommerce plugin designed to help modify the displayed address information on each order.
+ * Plugin Name: Show Modal To Unregistered Users
+ * Plugin URI:  https://github.com/chigozieorunta/wp-show-modal-to-unregistered-users
+ * Description: A WordPress plugin that helps site owners display a dialog box to unregistered users who try to access certain areas that require registration.
  * Version:     1.0.0
  * Author:      Chigozie Orunta
  * Author URI:  https://github.com/chigozieorunta
  * License:     MIT
- * Text Domain: woo-modify-order-address-details
+ * Text Domain: wp-show-modal-to-unregistered-users
  * Domain Path: ./
  */
 
 //Define Plugin Path
-define("WPMOAD", plugin_dir_path(__FILE__));
+define("WPSMUU", plugin_dir_path(__FILE__));
 
-wooModifyOrderAddressDetails::getInstance();
+wpShowModalToUnregisteredUsers::getInstance();
 
 /**
- * Class wooModifyOrderAddressDetails
+ * Class wpShowModalUnregisteredUsers
  */
-class wooModifyOrderAddressDetails {
+class wpShowModalToUnregisteredUsers {
     /**
 	 * Constructor
 	 *
 	 * @since  1.0.0
 	 */
     public function __construct() {
-        add_action('admin_enqueue_scripts', array(get_called_class(), 'registerScripts'));
+        add_action('enqueue_scripts', array(get_called_class(), 'registerScripts'));
     }
 
     /**
@@ -36,8 +36,8 @@ class wooModifyOrderAddressDetails {
 	 * @since  1.0.0
 	 */
     public static function registerScripts() {
-		wp_register_style('woo-modify-order-address-details', plugin_dir_url(__FILE__).'css/woo-modify-order-address-details.css');
-		wp_enqueue_style('woo-modify-order-address-details');
+		wp_register_style('wp-show-modal-unregistered-users', plugin_dir_url(__FILE__).'js/wp-show-modal-unregistered-users.css');
+		wp_enqueue_style('wp-show-modal-unregistered-users');
     }
 
     /**
