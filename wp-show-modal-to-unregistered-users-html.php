ff8f0add60b32ebbe14f406b2916d5bf87ec3ecf
chigozieorunta <chigozieorunta@yahoo.com>
2020-01-28 01:38:58 +0100

    Added modal HTML template

---
+++ wp-show-modal-to-unregistered-users-html.php
@@ -0,0 +1,12 @@
+<section id="wpsmuu">
+	<div class="wpsmuu-fullscreen">
+		<div class="wpsmuu-fullscreen-child">
+			<div class="wpsmuu-modal">
+			    <h1>Sign Up</h1>
+				<p>We understand you're eager to start shopping right away but we need you to exercise a little patience with us as we put finishing touches to our site.</p>
+				<p>Meanwhile, please feel free to go through our blog and if you haven't signed up with us, click on the link below to get started. Thank you.</p>
+				<a class="wpsmuu" href="https://www.monafrique.com/sign-up">Sign Up</a>
+			</div>
+		</div>
+	</div>
+</section>
